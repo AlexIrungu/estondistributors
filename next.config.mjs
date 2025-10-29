5fbab3fc21d872b82abd4f73cdc5ae72a8510ed6
AlexIrungu <aleaxmuiruri@gmail.com>
2025-10-29 15:44:04 +0300

    deploying to vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProduction = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  // output: 'export',
-  distDir: 'out',
+  // Remove static export settings for Vercel
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -13,9 +9,6 @@ const nextConfig = {
       },
     ],
   },
-  trailingSlash: true,
-  basePath: isProduction ? '/estondistributors' : '',
-  assetPrefix: isProduction ? '/estondistributors/' : '',
   env: {
     NEXT_PUBLIC_EMAILJS_SERVICE_ID: process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
     NEXT_PUBLIC_EMAILJS_TEMPLATE_ID: process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
